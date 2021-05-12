e1a4e2da3f8273a433de9d493bf9d79dc51dc17c
Stefan Ober <StefanO@ppedv.de>
2021-05-12 14:33:27 +0200

    Skript mit optionaler Verbose Ausgabe

--- 03-SkriptExample/GetSecEvents.ps1
+++ 03-SkriptExample/GetSecEvents.ps1
@@ -10,6 +10,8 @@ param(
 [int]$Newest = 10
 )
 
+#Write-Verbose bietet eine Zusatzausgabe welche nur bei BEdarf mit -Verbose ausgegeben wird
+Write-Verbose -Message "Vom User wurde angegeben. $Logname, $Computername, $EventId, $Newest"
 Get-EventLog -LogName $LogName -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
 
 
