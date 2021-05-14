5feb344ff19397c7f13b3c581e15c537af222b27
Stefan Ober <StefanO@ppedv.de>
2021-05-14 14:20:59 +0200

    .Net Assemblies in PowerShell Laden

---
+++ 06-SkriptmitDLL/Out-Voice.ps1
@@ -0,0 +1,25 @@
+﻿[cmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateLength(5,200)]
+    [string]$Text
+)
+
+#Zum laden von DLL / Assemblies
+Add-Type -AssemblyName System.Speech
+
+#erstellen einer Instanz 
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+<#
+#Abfragen der installierten Sprachpakete
+($speak.GetInstalledVoices()).voiceinfo
+
+#Setzen eines installierten Sprachpaketes
+$speak.SelectVoice("Microsoft Zira Desktop")
+
+#>
+$speak.Rate = 0
+[System.Console]::Beep(500,300)
+$speak.Speak($Text)
+[System.Console]::Beep(2000,500)
